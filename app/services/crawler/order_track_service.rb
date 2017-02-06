b28a65307135c6671bb7b05a793c8ee7b3512f6d
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-02-06 20:52:18 -0200

    adding order tracking service

---
+++ app/services/crawler/order_track_service.rb
@@ -0,0 +1,52 @@
+module Crawler
+  class OrderTrackService
+    attr_reader :aliexpress, :browser, :log
+
+    def initialize(aliexpress, browser, logger)
+      @aliexpress = aliexpress
+      @browser = browser
+      @log = logger
+    end
+
+    def track_order(order)
+      puts "========= Authenticate user for track an order"
+
+      login
+      open_order_page order.aliexpress_number
+
+      sleep 5
+      shipping_table = @browser.table(class: 'TP_ShippingTable')
+      if shipping_table.exists?
+        tracking_number = shipping_table.td(class: 'no')
+        if tracking_number.exists?
+          puts "========= Order already shipped: Getting tracking number #{tracking_number}"
+          @log.add_message('Atualizando wordpress com código de rastreio do pedido '+ tracking_number)
+          order.mark_as_tracked tracking_number
+        else
+          puts "========= Order not shipped yet: order number #{order_number}"
+          @log.add_message('Não foi possível rastrear o pedido '+ order_number)
+        end
+      end
+
+      tracking_number
+    end
+
+    private
+    def login
+      AuthenticationService.new(aliexpress, browser, log).login
+    end
+
+    def build_order_url(order_number)
+      "https://trade.aliexpress.com/order_detail.htm?orderId=#{order_number}"
+    end
+
+    def open_order_page(order_number)
+      @browser.goto build_order_url(order_number)
+    end
+
+    def notify_wordpress(order_number, tracking_number)
+      #update_tracking_number_note
+    end
+
+  end
+end
